c7da898a546e71d3c7a56447851d01d3ea28e4aa
Matan Noam Shavit <matan@lot18.com>
2016-11-30 17:19:51 -0500

    starting a Discord Chat Bot

---
+++ client.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+
+from .config import TOKEN
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('cat'):
+        await client.send_message(message.channel, 'meow meow')
+
+client.run(TOKEN)
