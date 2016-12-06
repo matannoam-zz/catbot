e10c6acfcea0c9249e0d714812a91d0179241d39
Matan Noam Shavit <matannoam@gmail.com>
2016-12-05 21:21:50 -0500

    more responses

--- client.py
+++ client.py
@@ -1,23 +1,58 @@
+import logging
+import re
+
 import discord
 import asyncio
 
 from config import TOKEN
 
+
+logger = logging.getLogger('catbot')
+
 client = discord.Client()
+
+
+CANNED_RESPONSES = {
+    'kitten': 'mew mew',
+    'kittie': 'mew mew',
+    'kittez': 'mew mew',
+    'kittehz': 'mew mew',
+    'kitteh': 'mew mew',
+    'kitties': 'mew mew',
+    'kitty': 'mew mew',
+    'string': 'mew',
+    'yarn': 'mew',
+    'cream': 'mreow',
+    'milk': 'mreow',
+    'tuna': 'mreow',
+    'fish': 'mreow',
+    'prr': 'prrr prrr',
+    'purr': 'purrr',
+    'cats': 'meow meow',
+    'cat' : 'meow',
+}
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('Logged in as {} ({})'.format(
+        client.user.name, client.user.id))
 
 @client.event
 async def on_message(message):
     content = message.content
-    if (' cat ' in content or ' cats ' in content
-            or content.startswith('cat ') or content.startswith('cats ')
-            or content.endswith(' cat') or content.endswith(' cats')):
-        await client.send_message(message.channel, 'meow meow')
+    response = get_response(content)
+    if response is not None:
+        await client.send_message(message.channel, response)
 
-client.run(TOKEN)
+
+def get_response(message_content):
+    words = re.findall(r"[\w]+", message_content)
+    for keyword in CANNED_RESPONSES:
+        if keyword in words:
+            return CANNED_RESPONSES[keyword]
+    return None
+
+
+if __name__ == '__main__':
+    client.run(TOKEN)
